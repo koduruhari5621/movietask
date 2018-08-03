493dec6ecb1aba56813c0fb144cd3577b0218580
amarnath <t.amarnath@hotmail.com>
2018-08-03 17:52:11 +0530

    mysql

---
+++ src/main/java/com/stackroute/movieservice/repository/MovieRepository.java
@@ -0,0 +1,14 @@
+package com.stackroute.movieservice.repository;
+
+import java.util.List;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.stackroute.movieservice.domain.Movie;
+
+@Repository
+public interface MovieRepository extends CrudRepository<Movie,Integer> {
+	
+
+}
